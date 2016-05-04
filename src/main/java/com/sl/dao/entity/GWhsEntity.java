92789ce04273bcffb998b328a2609021f0902807
sl521100 <sl521100@gmail.com>
2016-05-04 14:55:51 +0800

    commit

---
+++ src/main/java/com/sl/dao/entity/GWhsEntity.java
@@ -0,0 +1,219 @@
+package com.sl.dao.entity;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+public class GWhsEntity {
+/*<AUTOGEN--BEGIN>*/
+
+    /*<记录ID>*/
+    private BigDecimal recid;
+
+    /*<运单在库位上的件数>*/
+    private Integer pcs;
+
+    /*<运单在库位上的重量>*/
+    private BigDecimal weight;
+
+    /*<进出港标志（I--IMPORT,E-EXPORT）>*/
+    private String ei;
+
+    /*<运单在库位上的体积>*/
+    private BigDecimal vol;
+
+    /*<uld信息>*/
+    private String uldNo;
+
+    /*<仓库库位信息>*/
+    private String whslocation;
+
+    /*<运单记录主体ID>*/
+    private BigDecimal mawbid;
+
+    /*<运单前缀>*/
+    private String awbpre;
+
+    /*<运单号>*/
+    private String awbno;
+
+    /*<创建人>*/
+    private String createOpe;
+
+    /*<创建时间>*/
+    private Date createTime;
+
+    /*<更新人>*/
+    private String updateOpe;
+
+    /*<更新时间>*/
+    private Date updateTime;
+
+    /*<公司代码>*/
+    private String companyCode;
+
+    /*<备注>*/
+    private String remark;
+
+    /*<扩展字段1>*/
+    private String ext1;
+
+    /*<扩展字段2>*/
+    private String ext2;
+
+    /*<部门ID>*/
+    private BigDecimal deptid;
+
+    public BigDecimal getRecid() {
+        return recid;
+    }
+
+    public void setRecid(BigDecimal recid) {
+        this.recid = recid;
+    }
+
+    public Integer getPcs() {
+        return pcs;
+    }
+
+    public void setPcs(Integer pcs) {
+        this.pcs = pcs;
+    }
+
+    public BigDecimal getWeight() {
+        return weight;
+    }
+
+    public void setWeight(BigDecimal weight) {
+        this.weight = weight;
+    }
+
+    public String getEi() {
+        return ei;
+    }
+
+    public void setEi(String ei) {
+        this.ei = ei == null ? null : ei.trim();
+    }
+
+    public BigDecimal getVol() {
+        return vol;
+    }
+
+    public void setVol(BigDecimal vol) {
+        this.vol = vol;
+    }
+
+    public String getUldNo() {
+        return uldNo;
+    }
+
+    public void setUldNo(String uldNo) {
+        this.uldNo = uldNo == null ? null : uldNo.trim();
+    }
+
+    public String getWhslocation() {
+        return whslocation;
+    }
+
+    public void setWhslocation(String whslocation) {
+        this.whslocation = whslocation == null ? null : whslocation.trim();
+    }
+
+    public BigDecimal getMawbid() {
+        return mawbid;
+    }
+
+    public void setMawbid(BigDecimal mawbid) {
+        this.mawbid = mawbid;
+    }
+
+    public String getAwbpre() {
+        return awbpre;
+    }
+
+    public void setAwbpre(String awbpre) {
+        this.awbpre = awbpre == null ? null : awbpre.trim();
+    }
+
+    public String getAwbno() {
+        return awbno;
+    }
+
+    public void setAwbno(String awbno) {
+        this.awbno = awbno == null ? null : awbno.trim();
+    }
+
+    public String getCreateOpe() {
+        return createOpe;
+    }
+
+    public void setCreateOpe(String createOpe) {
+        this.createOpe = createOpe == null ? null : createOpe.trim();
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public String getUpdateOpe() {
+        return updateOpe;
+    }
+
+    public void setUpdateOpe(String updateOpe) {
+        this.updateOpe = updateOpe == null ? null : updateOpe.trim();
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    public String getCompanyCode() {
+        return companyCode;
+    }
+
+    public void setCompanyCode(String companyCode) {
+        this.companyCode = companyCode == null ? null : companyCode.trim();
+    }
+
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark == null ? null : remark.trim();
+    }
+
+    public String getExt1() {
+        return ext1;
+    }
+
+    public void setExt1(String ext1) {
+        this.ext1 = ext1 == null ? null : ext1.trim();
+    }
+
+    public String getExt2() {
+        return ext2;
+    }
+
+    public void setExt2(String ext2) {
+        this.ext2 = ext2 == null ? null : ext2.trim();
+    }
+
+    public BigDecimal getDeptid() {
+        return deptid;
+    }
+
+    public void setDeptid(BigDecimal deptid) {
+        this.deptid = deptid;
+    }
+
+/*<AUTOGEN--END>*/
+}
---
+++ src/main/java/com/sl/tools/MymybatisTool.java
@@ -0,0 +1,255 @@
+package com.sl.tools;
+
+import org.apache.tools.ant.util.StringUtils;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Created by shilin on 2016/5/3.
+ */
+public class MymybatisTool {
+    private static final String s1 = "/*<AUTOGEN--BEGIN>*/";
+    private static final String s2 = "/*<AUTOGEN--END>*/";
+
+    private static final String s3 = "<!--AUTOGEN-BEGIN-->";
+    private static final String s4 = "<!--AUTOGEN-END-->";
+
+    public static void main(String[] args) {
+        String path = "";
+        String targetPath = "";
+        String flag = "";
+        if (args.length > 2) {
+            path = args[0];
+            targetPath = args[1];
+            flag = args[2];
+        }
+//test
+        path = "D:\\Programe\\java\\maven\\mybatis-generator-1.3.2-source\\mybatis-gen-gradle\\src\\main\\java\\com\\sl\\dao";
+        targetPath = "D:\\Programe\\java\\maven\\mybatis-generator-1.3.2-source\\mybatis-gen-gradle\\test\\lgha;" +
+                "D:\\Programe\\java\\maven\\mybatis-generator-1.3.2-source\\mybatis-gen-gradle\\test\\mybatis";
+        flag = "UPDATE";
+
+        if (flag.equals("ADD")) {
+            invokeAddComment(path);
+        } else if (flag.equals("UPDATE")){
+            invokeUpdateComment(path, targetPath);
+        }
+
+    }
+
+    private static void invokeAddComment(String path) {
+        List<File> listJava = new ArrayList<File>();
+        List<File> listXml = new ArrayList<File>();
+
+        String[] pathArr = path.split(";");
+        for (String item : pathArr) {
+            File dir = new File(item);
+            getFileList(dir, ".java", listJava);
+            getFileList(dir, ".xml", listXml);
+        }
+
+
+        String b1 = "(public[\\S\\s]*?\\{)";
+
+
+        for (File item : listJava) {
+            String str = readFileContent(item);
+            boolean isWrite = false;
+            if (0 < str.indexOf(s1)) continue;
+
+            Matcher m = Pattern.compile(b1).matcher(str);
+            if (m.find()) {
+                str = m.replaceFirst(m.group() + "\n" + s1 + "\n");
+                isWrite = true;
+            }
+
+
+            int last = str.lastIndexOf("}");
+            if (0 <= last) {
+                str = str.substring(0, last) + "\n" + s2 + "\n" + str.substring(last);
+                isWrite = true;
+            }
+            if (isWrite) {
+                writeFileContent(str, item);
+                System.out.println("java文件加标识："+item.getAbsolutePath());
+            }
+        }
+
+        for (File item : listXml) {
+            String str = readFileContent(item);
+
+            if (0 < str.indexOf(s3)) continue;
+
+            boolean isWrite = false;
+            Matcher m = Pattern.compile("(<mapper[\\s\\S]*?>)").matcher(str);
+            if (m.find()) {
+                str = m.replaceFirst(m.group() + "\n" + s3 + "\n");
+                isWrite = true;
+            }
+            m = Pattern.compile("(</mapper[\\s\\S]*?>)").matcher(str);
+            if (m.find()) {
+                str = m.replaceFirst("\n" + s4 + "\n" + m.group());
+                isWrite = true;
+            }
+            if (isWrite) {
+                writeFileContent(str, item);
+                System.out.println("xml文件加标识："+item.getAbsolutePath());
+            }
+
+        }
+    }
+
+    private static String getMatchString(String regex, String content) {
+        Matcher m = Pattern.compile(regex).matcher(content);
+        if (m.find()) {
+            return m.group();
+        }
+        return "";
+    }
+
+    private static String getReplaceString(String regex, String content, String replaceContent) {
+        Matcher m = Pattern.compile(regex).matcher(content);
+        if (m.find()) {
+            return m.replaceFirst(replaceContent);
+        }
+        return "";
+    }
+
+    private static void invokeUpdateComment(String path, String targetPath) {
+        invokeAddComment(path);//先添加注释
+
+        List<File> listTargetJava = new ArrayList<File>();
+        List<File> listTargetXml = new ArrayList<File>();
+        String[] pathArr = targetPath.split(";");
+        for (String item : pathArr) {
+            File dir = new File(item);
+            getFileList(dir, ".java", listTargetJava);
+            getFileList(dir, ".xml", listTargetXml);
+        }
+
+
+        List<File> listJava = new ArrayList<File>();
+        List<File> listXml = new ArrayList<File>();
+        File dir = new File(path);
+        getFileList(dir, ".java", listJava);
+        getFileList(dir, ".xml", listXml);
+
+
+        String b1 = "(/\\*<AUTOGEN--BEGIN>\\*/[\\s\\S]*/\\*<AUTOGEN--END>\\*/)";
+        String b2 = "(<!--AUTOGEN-BEGIN-->[\\s\\S]*<!--AUTOGEN-END-->)";
+
+        for (File item : listJava) {
+            String str = readFileContent(item);
+            boolean isWrite = false;
+            if (str.indexOf(s1) < 0) continue;
+
+            for (File f : listTargetJava) {
+                if (f.getName().equals(item.getName())) {
+
+                    String findStr = getMatchString(b1, str);
+                    if (findStr != null && !findStr.equals("")) {
+                        String tarStr = readFileContent(f);
+                        if (tarStr != null && !tarStr.equals("")) {
+                            tarStr = getReplaceString(b1, tarStr, findStr);
+                            writeFileContent(tarStr, f);
+
+                            System.out.println("java文件更新：" + f.getAbsolutePath());
+                        }
+                    }
+
+                    break;
+                }
+            }
+        }
+
+        for (File item : listXml) {
+            String str = readFileContent(item);
+            boolean isWrite = false;
+            if (str.indexOf(s3) < 0) continue;
+
+            for (File f : listTargetXml) {
+                if (f.getName().equals(item.getName())) {
+
+                    String findStr = getMatchString(b2, str);
+                    if (findStr != null && !findStr.equals("")) {
+                        String tarStr = readFileContent(f);
+                        if (tarStr != null && !tarStr.equals("")) {
+                            tarStr = getReplaceString(b2, tarStr, findStr);
+                            writeFileContent(tarStr, f);
+
+                            System.out.println("xml文件更新："+f.getAbsolutePath());
+
+                        }
+                    }
+
+                    break;
+                }
+            }
+        }
+    }
+
+    private static String readFileContent(File item) {
+        String content = "";
+        BufferedReader br = null;
+        StringBuilder sb = new StringBuilder();
+        try {
+            br = new BufferedReader(new InputStreamReader(new FileInputStream(item)));
+            String str = "";
+            while ((str = br.readLine()) != null) {
+                sb.append(str + "\n");
+            }
+            content = sb.toString();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (br != null)
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+        }
+        return content;
+    }
+
+    private static String writeFileContent(String content, File item) {
+
+        BufferedWriter br = null;
+        try {
+            br = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(item)));
+            br.write(content);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (br != null)
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+        }
+        return content;
+    }
+
+    private static void getFileList(File file, final String suffix, List<File> listFiles) {
+        List<File> resultList = new ArrayList<File>();
+        File dir = file;
+        if (dir.isDirectory()) {
+            for (File item : dir.listFiles()) {
+                if (item.isDirectory()) {
+                    getFileList(item, suffix, listFiles);
+                } else {
+                    if (item.getName().endsWith(suffix)) {
+                        listFiles.add(item);
+                    }
+                }
+            }
+        }
+    }
+
+}
